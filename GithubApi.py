96fd188314386609db0187a625cc7d3882ff1f2f
alex <mifi123456@mail.ru>
2021-08-05 01:05:30 +0300

    workBranch

--- GithubApi.py
+++ GithubApi.py
@@ -4,18 +4,19 @@ import json
 import os
 
 class GithubApi:
-    def __init__(self, url, user, token, repo):
+    def __init__(self, url, user, token, repo, workBranch):
         self.url = url
         self.user = user
         self.token = token
         self.repo = repo
+        self.workBranch = workBranch
 
     def commitPush(self):
         # Получаем ссылку на последний коммит
-        ref = requests.get('{0}/repos/{1}/{2}/git/matching-refs/heads/master'.format(self.url, self.user, self.repo), auth=(self.user, self.token), headers={"Accept" : "application/vnd.github.v3+json"})
+        ref = requests.get('{0}/repos/{1}/{2}/git/matching-refs/heads/{3}'.format(self.url, self.user, self.repo, self.workBranch), auth=(self.user, self.token), headers={"Accept" : "application/vnd.github.v3+json"})
         # Получаем хэш последнего коммита
         shaHead = ref.json()[0]['object']['sha']
-        # Получаем инфу о последнем коммите в ветке master
+        # Получаем инфу о последнем коммите в ветке workBranch
         commitHead = requests.get('{0}/repos/{1}/{2}/git/commits/{3}'.format(self.url, self.user, self.repo, shaHead), auth=(self.user, self.token), headers={"Accept" : "application/vnd.github.v3+json"})
         # Получаем хэш дерева
         shaTree = commitHead.json()['tree']['sha'] 
@@ -50,9 +51,9 @@ class GithubApi:
         shaNewCommit = commitRespons.json()['sha']
 
         updateInfo = {"sha": shaNewCommit}
-        updateResponse = requests.post('{0}/repos/{1}/{2}/git/refs/heads/master'.format(self.url, self.user, self.repo), json=updateInfo, auth=(self.user, self.token), headers={"Accept" : "application/vnd.github.v3+json"})
+        updateResponse = requests.post('{0}/repos/{1}/{2}/git/refs/heads/{3}'.format(self.url, self.user, self.repo, self.workBranch), json=updateInfo, auth=(self.user, self.token), headers={"Accept" : "application/vnd.github.v3+json"})
         
-        #curl -u AlexBaturo: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  -X PATCH   -H "Accept: application/vnd.github.v3+json"   https://api.github.com/repos/AlexBaturo/Jenkins/git/refs/heads/master   -d '{"sha":"90f5a1500731930765652a98623cf650624a9214"}'
+        #curl -u AlexBaturo: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  -X PATCH   -H "Accept: application/vnd.github.v3+json"   https://api.github.com/repos/AlexBaturo/Jenkins/git/refs/heads/dev   -d '{"sha":"90f5a1500731930765652a98623cf650624a9214"}'
         #Обновление коммита
 
     def getChangedFiles(self):
@@ -67,9 +68,10 @@ class GithubApi:
 
 url = "https://api.github.com"
 user = "AlexBaturo"
-token = "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"
+token = "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"
 repo = "Jenkins"
+workBranch = "master"
 
-test = GithubApi(url, user, token, repo)
+test = GithubApi(url, user, token, repo, workBranch)
 test.commitPush()
 #test.getChangedFiles()
