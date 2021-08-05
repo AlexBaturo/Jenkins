859ff51dd6578a2c0ca743b2f2192fedb24ca926
alex <mifi123456@mail.ru>
2021-08-05 23:06:30 +0300

    format

--- GithubApi.py
+++ GithubApi.py
@@ -4,19 +4,25 @@ import json
 import os
 
 class GithubApi:
-    def __init__(self, url, user, token, repo):
+    def __init__(self, url, user, token):
         self.url = url
         self.user = user
         self.token = token
-        self.repo = repo
+        self.header = {"Accept" : "application/vnd.github.v3+json"}
+        self.prefix = '{0}/repos/{1}'.format(self.url, self.user)
 
-    def commitPush(self, workBranch):
+    def commitPush(self, workBranch, repo):
+
         # Получаем ссылку на последний коммит
-        ref = requests.get('{0}/repos/{1}/{2}/git/matching-refs/heads/{3}'.format(self.url, self.user, self.repo, workBranch), auth=(self.user, self.token), headers={"Accept" : "application/vnd.github.v3+json"})
+        ref = requests.get('{0}/{1}/git/matching-refs/heads/{2}'.format(self.prefix, repo, workBranch), 
+                             auth=(self.user, self.token),
+                             headers=self.header)
         # Получаем хэш последнего коммита
         shaHead = ref.json()[0]['object']['sha']
         # Получаем инфу о последнем коммите в ветке workBranch
-        commitHead = requests.get('{0}/repos/{1}/{2}/git/commits/{3}'.format(self.url, self.user, self.repo, shaHead), auth=(self.user, self.token), headers={"Accept" : "application/vnd.github.v3+json"})
+        commitHead = requests.get('{0}/{1}/git/commits/{2}'.format(self.prefix, repo, shaHead),
+                                    auth=(self.user, self.token), 
+                                    headers=self.header)
         # Получаем хэш дерева
         shaTree = commitHead.json()['tree']['sha'] 
         
@@ -28,39 +34,54 @@ class GithubApi:
         for file in filesList:
 
             changedFilesData = {"content":  open(file,"r").read()}  
-            blob = requests.post('{0}/repos/{1}/{2}/git/blobs'.format(self.url, self.user, self.repo), json=changedFilesData, auth=(self.user, self.token), headers={"Accept" : "application/vnd.github.v3+json"})
+            blob = requests.post('{0}/{1}/git/blobs'.format(self.prefix, repo),
+                                  json=changedFilesData,
+                                  auth=(self.user, self.token),
+                                  headers=self.header)
+
             fileShaBlobDict[file] = blob.json()['sha']
         # Создание информации о файлах
         changedFilesData = {"tree" : []}
         for file, shaBlob in fileShaBlobDict.items():
-            changedFilesData["tree"].append({"path":file,"mode":"100644","type":"blob","sha": shaBlob})
+            changedFilesData["tree"].append({"path":file,
+                                             "mode":"100644",
+                                             "type":"blob",
+                                             "sha": shaBlob})
 
         # инфа дерева последнего коммита
-        tree = requests.get('{0}/repos/{1}/{2}/git/trees/{3}'.format(self.url, self.user, self.repo, shaTree), auth=(self.user, self.token), headers={"Accept" : "application/vnd.github.v3+json"})
+        tree = requests.get('{0}/{1}/git/trees/{2}'.format(self.prefix, repo, shaTree), 
+                             auth=(self.user, self.token),
+                             headers=self.header)
         
         # создаем дерево
         #changedFilesData = {"tree":[{"path":"README.md","mode":"100644","type":"blob","sha": shaBlob1}, {"path":"testfile.jenkins","mode":"100644","type":"blob","sha": shaBlob2}]}
-        treeHead = requests.post('{0}/repos/{1}/{2}/git/trees'.format(self.url, self.user, self.repo), json=changedFilesData, auth=(self.user, self.token), headers={"Accept" : "application/vnd.github.v3+json"})
-        print(treeHead.json())
+        treeHead = requests.post('{0}/{2}/git/trees'.format(self.prefix, repo),
+                                  json=changedFilesData, auth=(self.user, self.token), 
+                                  headers=self.header)
+       
         shaTreeHead = treeHead.json()['sha']
-        #print(treeHead.json())
         #Создание коммита
         commitInfo = {"message":"from api","tree": shaTreeHead, "parents": [shaHead] }
-        commitRespons = requests.post('{0}/repos/{1}/{2}/git/commits'.format(self.url, self.user, self.repo), json=commitInfo, auth=(self.user, self.token), headers={"Accept" : "application/vnd.github.v3+json"})
+        commitRespons = requests.post('{0}/{2}/git/commits'.format(self.prefix, repo),
+                                       json=commitInfo, 
+                                       auth=(self.user, self.token),
+                                       headers=self.header)
+
         shaNewCommit = commitRespons.json()['sha']
 
         updateInfo = {"sha": shaNewCommit}
-        updateResponse = requests.post('{0}/repos/{1}/{2}/git/refs/heads/{3}'.format(self.url, self.user, self.repo, workBranch), json=updateInfo, auth=(self.user, self.token), headers={"Accept" : "application/vnd.github.v3+json"})
+        updateResponse = requests.post('{0}/{2}/git/refs/heads/{3}'.format(self.prefix, repo, workBranch),
+                                        json=updateInfo,
+                                        auth=(self.user, self.token),
+                                        headers=self.header)
         
-        #curl -u AlexBaturo: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  -X PATCH   -H "Accept: application/vnd.github.v3+json"   https://api.github.com/repos/AlexBaturo/Jenkins/git/refs/heads/dev   -d '{"sha":"90f5a1500731930765652a98623cf650624a9214"}'
-        #Обновление коммита
-
-
-
     def pullRequest(self, workBranch):
+
         pullRequestInfo = {"head": workBranch,"base":"master", "title" : "fromApi"}
-        pullRequestRespons = requests.post('{0}/repos/{1}/{2}/pulls'.format(self.url, self.user, self.repo), json=pullRequestInfo, auth=(self.user, self.token), headers={"Accept" : "application/vnd.github.v3+json"})
-        print(pullRequestRespons.json())
+        pullRequestRespons = requests.post('{0}/{2}/pulls'.format(self.prefix, repo),
+                                            json=pullRequestInfo,
+                                            auth=(self.user, self.token),
+                                            headers=self.header)
 
     def __getProjectFiles(self):
         folder = []
@@ -77,10 +98,10 @@ class GithubApi:
 
 url = "https://api.github.com"
 user = "AlexBaturo"
-token = "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"
+token = "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"
 repo = "Jenkins"
 workBranch = "dev"
 
-test = GithubApi(url, user, token, repo)
-test.commitPush(workBranch)
-test.pullRequest(workBranch)
+test = GithubApi(url, user, token)
+test.commitPush(workBranch, repo)
+test.pullRequest(workBranch, repo)
